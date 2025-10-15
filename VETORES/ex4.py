d0a2779f5ecfc5bf4498201269b8093a9e09a56a
miguel-zacharias <miguel.zacharias@aluno.senai.br>
2025-10-15 14:45:54 -0300

    adsfgdh

--- VETORES/ex4.py
+++ VETORES/ex4.py
@@ -4,11 +4,11 @@ for i in range(5):
     nome = input(f"Digite o {i+1}o nome: ")
     nomes.append(nome)
 
-busca = input("Digite um nome para buscar: ")
+busc = input("Digite um nome para buscar: ")
 
-if busca in nomes:
-    print(f'O nome "{busca}" está na lista.')
+if busc in nomes:
+    print(f'O nome "{busc}" está na lista.')
 else:
-    print(f'O nome "{busca}" não está na lista.')
+    print(f'O nome "{busc}" não está na lista.')
 
 #feitoporzacharias
