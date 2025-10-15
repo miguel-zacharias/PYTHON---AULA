f52145ea868c47f8d52eaef9f4d9b988b95cef3d
miguel-zacharias <miguel.zacharias@aluno.senai.br>
2025-10-15 14:45:09 -0300

    asdfgshd

--- OPERADORES/ex.1.py
+++ OPERADORES/ex.1.py
@@ -8,14 +8,14 @@ num2 = int(input("Digita seu segundo número: "))
 
 soma = num1 + num2
 subtrai = num1 - num2
-multiplica = num1 * num2
+mutiplica = num1 * num2
 divide = num1 / num2
 rest_div = num1 % num2
 poten = num1 ** num2
 
 print(f"A soma é: {soma}")
 print(f"A subtração é: {subtrai}")
-print(f"A multiplicação é: {multiplica}")
+print(f"A multiplicação é: {mutiplica}")
 print(f"A divisão é: {divide}")
 print(f"O resto da divisão é: {rest_div}")
 print(f"A potência é: {poten}")
